504c37de3dd10c645bb7404d73e1c5ca3ee88561
Deceitful <gfelipe0555@gmail.com>
2024-07-05 18:22:45 -0300

    Inicialização do projeto springboot

---
+++ springboot3-project/src/main/java/course/com/springboot3project/Springboot3ProjectApplication.java
@@ -0,0 +1,16 @@
+package course.com.springboot3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3ProjectApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Springboot3ProjectApplication.class, args);
+
+    }
+
+}
---
+++ springboot3-project/src/test/java/course/com/springboot3project/Springboot3ProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package course.com.springboot3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3ProjectApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
