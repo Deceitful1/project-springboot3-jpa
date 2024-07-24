2a30b96f08cbb3a76b7279fae1a9ade34e79fa3a
Deceitful <gfelipe0555@gmail.com>
2024-07-24 11:47:55 -0300

    update user

--- springboot3-project/src/main/java/course/services/UserService.java
+++ springboot3-project/src/main/java/course/services/UserService.java
@@ -24,23 +24,38 @@ public class UserService
         Optional<User> obj = userRepository.findById(id);
         return obj.get();
     }
+
     public User findByName(String name)
     {
         Optional<User> obj = Optional.ofNullable(userRepository.findByName(name));
         return obj.get();
     }
+
     public User insert(User user)
     {
         userRepository.save(user);
         return user;
     }
+
     public void delete(Long id)
     {
         userRepository.deleteById(id);
     }
 
+    public User update(Long id, User obj)
+    {
+        User entity = userRepository.getReferenceById(id);
+        updateData(entity, obj);
+        return userRepository.save(entity);
+    }
 
+    private void updateData(User entity, User obj)
+    {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
 
+    }
 
 
 }
